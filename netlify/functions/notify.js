c74342e6211d58f4fa76a72cfa2d2606ca62461b
tuhin <tuhin.thakur1233@gmail.com>
2026-01-24 16:27:23 +0530

    fix noti

--- netlify/functions/notify.js
+++ netlify/functions/notify.js
@@ -3,11 +3,15 @@ const admin = require("firebase-admin");
 
 // Initialize Firebase Admin (using environment variables for security)
 if (!admin.apps.length) {
+  const privateKey = process.env.FIREBASE_PRIVATE_KEY
+    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+    : undefined;
+
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.VITE_FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      privateKey: privateKey,
     }),
   });
 }
